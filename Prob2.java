3ba2e77a13a72abe44ebdf730826b4b385ae5de2
Simi <73349362+idioticmasculum@users.noreply.github.com>
2021-01-19 15:18:40 +0000

    finished problem 3

--- Prob2.java
+++ Prob2.java
@@ -3,5 +3,12 @@ import java.util.Scanner;
 class Prob2 {
   public static void main(String[] args) {
     
+  Scanner scan = new Scanner(System.in);
+  System.out.println("Enter a string");
+  String word = scan.nextLine();
+ 
+  System.out.println(word.substring(0,1));
+
+  System.out.println(word.length(-1));
   }
 }
